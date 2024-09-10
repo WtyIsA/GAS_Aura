8713ac7be078a7e1ae96290fa3dee222e614c322
WtyIsA <48829444+WtyIsA@users.noreply.github.com>
2024-09-10 22:45:24 +0800

    commit Unlua

---
+++ Content/Script/GAS/GA/GA_TestGA.lua
@@ -0,0 +1,17 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+---@type GA_TestGA_C
+local M = UnLua.Class()
+
+
+function M:ActivateAbility()
+    print("GA_TestGA_C ActivateAbility")
+end
+
+return M
